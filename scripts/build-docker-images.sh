a27b830e82be02754676c89392d52723682f9c7c
ryanschuhler <ryan.schuhler@liferay.com>
2018-04-20 15:55:58 -0700

    Remove runtime image

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -67,7 +67,6 @@ docker build \
 	--build-arg DEPLOYMENT_ITEMS=hooks/osb-www-hook,hooks/portal-compat-hook,portlets/osb-www-asset-publisher-portlet,portlets/hubspot-portlet,themes/osb-community-theme,portlets/osb-www-marketing-events-portlet,portlets/marketplace-portlet,portlets/osb-www-portlet,webs/elasticsearch-web,themes/osb-www-events-theme \
 	--build-arg GIT_BRANCH=ee-6.2.x \
 	--build-arg GIT_REPO=git@github.com:liferay/liferay-plugins-ee.git \
-	--build-arg RUNTIME_IMAGE=alpine:3.4 \
 	../liferay-instances
 
 docker build \
@@ -79,5 +78,4 @@ docker build \
 	--build-arg DEPLOYMENT_ITEMS=ext/osb-ext,hooks/kaleo-upgrade-hook,hooks/osb-hook,hooks/portal-compat-hook,portlets/akismet-portlet,portlets/audit-portlet,portlets/ddl-form-portlet,portlets/ecommerce-portlet,portlets/google-search-appliance-portlet,portlets/hubspot-portlet,portlets/ip-geocoder-portlet,portlets/my-subscriptions-portlet,portlets/oauth-portlet,portlets/osb-corp-profile-portlet,portlets/osb-marketplace-order-portlet,portlets/osb-marketplace-order-portlet,portlets/osb-partnership-portlet,portlets/osb-portlet,portlets/osb-salesforce-gateway-portlet,portlets/osb-ui-portlet,portlets/rabbitmq-portlet,portlets/release-notes-portlet,portlets/saml-portlet,portlets/social-coding-portlet,portlets/social-networking-portlet,portlets/token-auth-portlet,portlets/web-form-portlet,themes/osb-community-theme,themes/osb-conference-theme,themes/osb-ray-by-day-theme,themes/osb-support-theme,themes/osb-symposium-2013-theme,themes/osb-symposium-2014-theme,themes/osb-universal-theme,themes/osb-welcome-theme,themes/osb-www-theme,webs/ehcache-cluster-web,webs/elasticsearch-web,webs/jira-web,webs/kaleo-web,webs/osb-blogs-web,webs/skinny-web,webs/vldap-web \
 	--build-arg GIT_BRANCH=ee-6.1.x \
 	--build-arg GIT_REPO=git@github.com:liferay/liferay-plugins-ee.git \
-	--build-arg RUNTIME_IMAGE=alpine:3.4 \
 	../liferay-instances
