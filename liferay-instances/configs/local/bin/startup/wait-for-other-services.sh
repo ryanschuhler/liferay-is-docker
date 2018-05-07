cb96d122941ec30214b2f316ad7f55c905673cb6
ryanschuhler <ryan.schuhler@liferay.com>
2018-05-07 15:33:05 -0700

    More updates

--- liferay-instances/configs/local/bin/startup/wait-for-other-services.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-chmod u+x ${LIFERAY_HOME}/bin/wait-for-it.sh
-bash ${LIFERAY_HOME}/bin/wait-for-it.sh -t 600 ${DB_CONTAINER}:3306
-bash ${LIFERAY_HOME}/bin/wait-for-it.sh -t 600 ${SEARCH_CONTAINER}:9200
